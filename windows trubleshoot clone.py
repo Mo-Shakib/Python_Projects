d34a0ea843bccfa19aac462caa57482ef4c2a729
Mohammad Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-09-03 03:40:09 +0600

    Create windows trubleshoot clone.py

---
+++ windows trubleshoot clone.py
@@ -0,0 +1,10 @@
+import time
+print('Searching for problems',end='')
+for i in range(10):
+    time.sleep(1)
+    print('.',end='')
+
+time.sleep(5)
+print('\nTroubleshooting has completed')
+time.sleep(2)
+print('No problem found!')
