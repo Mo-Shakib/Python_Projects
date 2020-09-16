35501cf1ce76a8fb94f2552859b345d3b2aa7fd2
Shakib <code.shakb@gmail.com>
2020-09-16 20:34:18 +0600

    create executeTime file

---
+++ count_executeTime.py
@@ -0,0 +1,8 @@
+import time
+startTime = time.time()
+
+# write your code or function calls
+
+endTime = time.time()
+totalTime = endTime - startTime
+print('Time taken to execute code =', totalTime)
