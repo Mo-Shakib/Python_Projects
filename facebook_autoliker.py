dc1ef9f0f92f40ec101d92e07e7b4fa544d4837f
Shakib <code.shakb@gmail.com>
2020-09-16 20:30:57 +0600

    Update facebook_autoliker.py

--- facebook_autoliker.py
+++ facebook_autoliker.py
@@ -2,9 +2,9 @@ import pyautogui
 import time
 pyautogui.FAILSAFE = False
 for i in range(10):
-    time.sleep()
+    time.sleep(3)
     pyautogui.press('j')
-    pyautogui.press('l')
-    pyautogui.press('tab')
-    pyautogui.press('enter')
+    # pyautogui.press('l')
+    # pyautogui.press('tab')
+    # pyautogui.press('enter')
 print('Done')
