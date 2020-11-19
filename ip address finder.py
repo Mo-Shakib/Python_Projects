9889356d20741a7ffc6335964a8c04b3a2477a2a
Code-Shakib <imoshakib.bd@gmail.com>
2020-11-19 11:41:42 +0600

    Update ip address finder.py

--- ip address finder.py
+++ ip address finder.py
@@ -1,5 +1,5 @@
 # this script can find any website IP address
 import socket
-host = 'www.apple.com' # web address
+host = input('Enter URL: ') # web address
 ip = socket.gethostbyname(host)
 print(f'IP of {host} is {ip}')
