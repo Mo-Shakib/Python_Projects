3a449ffbaddb26930f06d620f0607a96ad10ddc6
Code-Shakib <imoshakib.bd@gmail.com>
2020-12-04 11:27:16 +0600

    ok

--- listComprehension.py
+++ listComprehension.py
@@ -1,2 +1,12 @@
 evenNumbers = [x for x in range(1000) if x % 2 == 0]
 print(evenNumbers)
+if evenNumbers == 100:
+    print('hello')
+
+for i in range(100):
+    print('ok')
+
+
+a = 'apple'
+
+# instance variable added
